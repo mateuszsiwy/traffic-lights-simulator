1520fd5ac523b3cfdd70118ae5e1497e847faaed
msiwy12345 <mateuszsiwy00@gmail.com>
2025-03-26 22:24:33 +0100

    Initial commit

---
+++ backend/src/main/java/com/mateuszsiwy/traffic_ligths_simulator/TrafficLigthsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszsiwy.traffic_ligths_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLigthsSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficLigthsSimulatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mateuszsiwy/traffic_ligths_simulator/TrafficLigthsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszsiwy.traffic_ligths_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLigthsSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
